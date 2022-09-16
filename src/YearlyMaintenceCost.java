ac639f55e3b1050b7370ec445880f0c0d055d87a
leichtje <leichtje@mail.uc.edu>
2022-09-16 11:40:30 -0400

    Completed yearly maintence cost program

---
+++ src/YearlyMaintenceCost.java
@@ -0,0 +1,17 @@
+public class YearlyMaintenceCost
+{
+    public static void main(String[] args) {
+        double springCost=100.0;
+        double summerCost=75.55;
+        double fallCost=105.43;
+        double winterCost=99.99;
+        double totalYearlyMaintenceCost;
+
+
+
+
+        totalYearlyMaintenceCost=springCost+summerCost+fallCost+winterCost;
+
+        System.out.println("The total yearly maintence cost is:" + totalYearlyMaintenceCost);
+    }
+}
